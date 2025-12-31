2974bc3c2c30418ddd51c3a938fde4d3b6ad2e20
dmori <chosehoon2@naver.com>
2025-12-31 23:39:00 +0900

    10871: 브론즈5, 재도전(StringBuilder로 모아서 한 번에 출력)

---
+++ src/implementation/Problem10871Retry.java
@@ -0,0 +1,26 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem10871Retry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input1 = br.readLine().split(" ");
+        int n = Integer.parseInt(input1[0]);
+        int x = Integer.parseInt(input1[1]);
+
+        String[] input2 = br.readLine().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(input2[i]);
+            if (num < x) {
+                sb.append(num).append(" ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
