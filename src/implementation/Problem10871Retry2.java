8b5fb1d89d65c4cea5e45afa9c175b4312f32322
dmori <chosehoon2@naver.com>
2025-12-31 23:52:03 +0900

    10871, 재도전(Stream으로 구현)

---
+++ src/implementation/Problem10871Retry2.java
@@ -0,0 +1,23 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Problem10871Retry2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine().split(" ")[1]);
+
+        String answer = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .filter(num -> num < x)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(answer);
+    }
+}
