b19dc22f7bc97ad772268140e413e0f1a62f6670
dmori <chosehoon2@naver.com>
2026-01-03 14:29:49 +0900

    5597: 브론즈3, 구현, boolean 배열이 더 좋았을 거 같다!

---
+++ src/implementation/Problem5597.java
@@ -0,0 +1,25 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem5597 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] student = new int[31];
+
+        for(int i = 0; i < 28; i++){
+            int num = Integer.parseInt(br.readLine());
+            student[num] = 1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= 30; i++){
+            if(student[i] == 0){
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
