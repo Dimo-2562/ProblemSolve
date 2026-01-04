7030291ffb073dbc60c9833372545a1a82d8de54
dmori <chosehoon2@naver.com>
2026-01-04 15:40:58 +0900

    11720: 브론즈4, 숫자의 합 문자열 내 숫자의 변환

---
+++ src/string/Problem11720Retry.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem11720Retry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        int sum = 0;
+        for (char c : br.readLine().toCharArray()) {
+            sum += c - '0';
+        }
+        System.out.println(sum);
+    }
+}
