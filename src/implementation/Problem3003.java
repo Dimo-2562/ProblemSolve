52342a285799b7bcfbee458c293662fdc0c6297c
dmori <chosehoon2@naver.com>
2026-01-05 17:45:16 +0900

    3003: 브론즈5, 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ src/implementation/Problem3003.java
@@ -0,0 +1,27 @@
+package implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem3003 {
+    private static int[] arr = {
+            1, 1, 2, 2, 2, 8
+    };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = arr[i] - num;
+            sb.append(arr[i]).append(' ');
+        }
+
+        System.out.print(sb);
+
+    }
+}
