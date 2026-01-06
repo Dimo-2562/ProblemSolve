8bda8752c2f17ff5d8476e05ab047e85c14f16ff
dmori <chosehoon2@naver.com>
2026-01-06 18:55:42 +0900

    2941: 실버5, 크로아티아 알파벳 String의 replace를 활용

---
+++ src/silver/string/Problem2941.java
@@ -0,0 +1,20 @@
+package silver.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        String[] croatian = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (String s : croatian) {
+            input = input.replace(s, "*");
+        }
+
+        System.out.print(input.length());
+    }
+}
