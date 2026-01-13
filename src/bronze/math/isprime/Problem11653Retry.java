56396bf0a8946ddc6163a53fa33786c67ca4b810
dmori <chosehoon2@naver.com>
2026-01-14 00:15:12 +0900

    11653: 소인수분해(재도전), 브론즈1 체 제거하고 단순 소인수분해로 풀이

---
+++ src/bronze/math/isprime/Problem11653Retry.java
@@ -0,0 +1,26 @@
+package bronze.math.isprime;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem11653Retry {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                sb.append(i).append('\n');
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            sb.append(n);
+        }
+
+        System.out.print(sb);
+    }
+}
