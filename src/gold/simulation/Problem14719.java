d2c523661510b26dc40ede0b4c43856fbcc3f00d
dmori <chosehoon2@naver.com>
2026-04-07 18:43:01 +0900

    14719: 빗물, 골드5 시뮬레이션, 왼쪽과 오른쪽의 최대 높이를 체크

--- src/gold/simulation/Problem14719.java
+++ src/gold/simulation/Problem14719.java
@@ -1,10 +1,10 @@
-package later;
+package gold.simulation;
 
 import java.io.*;
 import java.util.*;
 
 /*
-고이는 빗물의 총량
+빗물의 총량을 구하시오.
 
 <입력>
 H, W
@@ -13,20 +13,21 @@ H, W
 높이 (W개)
 
 <출력>
-고이는 빗물의 총량을 출력하라.
+한 칸의 용량이 1일 때 빗물의 총량을 구하라.
+- 전혀 고이지 않을 때는 0을 출력하라.
 
-스택
-왼쪽에 높은 게 있고, 작은 것, 그리고 다시 높은 것 -> 물이 고임.
-
+<풀이>
+쌓이는 조건 -> 왼쪽의 높은 것과 오른쪽의 높은 것
+그리고 그 중 작은 것의 높이 - 내 높이 = 쌓이는 양
  */
 
-public class Problem14719Later {
+public class Problem14719 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int h = Integer.parseInt(st.nextToken());
         int w = Integer.parseInt(st.nextToken());
-
         int[] height = new int[w];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < w; i++) {
@@ -35,21 +36,22 @@ public class Problem14719Later {
 
         int[] leftMax = new int[w];
         leftMax[0] = height[0];
+        int[] rightMax = new int[w];
+        rightMax[w - 1] = height[w - 1];
+
         for (int i = 1; i < w; i++) {
             leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
-
-        int[] rightMax = new int[w];
-        rightMax[w - 1] = height[w - 1];
         for (int i = w - 2; i >= 0; i--) {
             rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
 
-        int answer = 0;
-        for (int i = 0; i < w; i++) {
-            answer += Math.min(rightMax[i], leftMax[i]) - height[i];
+        int sum = 0;
+        for (int i = 1; i <= w - 2; i++) {
+            int max = Math.min(leftMax[i], rightMax[i]);
+            if (max > height[i]) sum += max - height[i];
         }
 
-        System.out.print(answer);
+        System.out.print(sum);
     }
 }
