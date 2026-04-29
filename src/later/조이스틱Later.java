b4ce1c0110f1bb9d3be3d6108aace1e67423720f
dmori <chosehoon2@naver.com>
2026-04-29 22:25:12 +0900

    조이스틱, Lv2 그리디, 각 알파벳에서 연속한 A만큼 건너뛰고 왼쪽으로 돌지 오른쪽으로 돌지 결정

---
+++ src/later/조이스틱Later.java
@@ -0,0 +1,33 @@
+package later;
+
+import java.util.*;
+
+/*
+
+ */
+
+class 조이스틱Later {
+    public int solution(String name) {
+        int n = name.length();
+        int answer = 0;
+
+        int move = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            char c = name.charAt(i);
+
+            answer += Math.min(c - 'A', 'Z' - c + 1);
+
+            int next = i + 1;
+
+            while (next < n && name.charAt(next) == 'A') {
+                next++;
+            }
+
+            move = Math.min(move, i * 2 + (n - next));
+            move = Math.min(move, i + 2 * (n - next));
+        }
+
+        return answer + move;
+    }
+}
